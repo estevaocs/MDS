edb1857ce69d6c4443ceb3a0d37e15f9f0d3f9b6
Estevao Cristino <estevao_cs@hotmail.com>
2016-09-17 17:11:26 -0300

    adicao do exercicio 4 da lista 2

---
+++ lista2/exercicio4/src/exercicio4/Agenda.java
@@ -0,0 +1,36 @@
+package exercicio4;
+
+/**
+ *
+ * @author Estevao
+ */
+public class Agenda {
+
+    private String nome;
+    private String telefone;
+
+    public Agenda(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public String getContato(){
+        return ("nome: " + this.nome +"\ntelefone: " + this.telefone);
+    }
+}
